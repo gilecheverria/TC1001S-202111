49e043f8da905257e6073844b04358429b147dc7
gilecheverria <gilbertoef@gmail.com>
2021-05-06 09:11:07 -0500

    Add example of turtle code

---
+++ Python/test_turtle.py
@@ -0,0 +1,14 @@
+"""
+Simple example of tutle instructions
+
+Gilberto Echeverria
+2021-05-06
+"""
+
+import turtle
+
+for i in range(8):
+    turtle.forward(100)
+    turtle.right(45)
+
+turtle.done()
